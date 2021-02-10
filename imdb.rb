5c288b978945d186de19ce9cf44ad8c1741d649e
koremori <ifyourelookingforhotwater@gmail.com>
2021-02-10 19:31:28 +0300

    rewrite with csv openstruct and date

--- imdb.rb
+++ imdb.rb
@@ -1,33 +1,46 @@
 # frozen_string_literal: true
 
-def reading_file(file_path)
-  default_path = '/home/bunta/Documents/rubycode/movies.txt'
+require 'csv'
+require 'ostruct'
+require 'date'
+
+def reading_file(file_path) #  =>  String
+  default_path = '/home/koremori/documents/rubycode/movies.txt'
   File.exist?(file_path) ? File.read(file_path) : File.read(default_path)
 end
 
-def normalizing_movie_data(raw_data)
-  raw_data.split("\n").map { |item| item.split('|') }
+def normalizing_movie_data(raw_data) # => multidimensional Array with Strings
+  CSV.parse(raw_data, col_sep: '|')
 end
 
-def movie_to_hash(movies)
+def movie_to_ostruct(movies) # => Array with OpenStruct
   movies.map do |movie|
-    {
-      link: movie[0],
-      title: movie[1],
-      shoot_year: movie[2].to_i,
-      origin_country: movie[3],
-      release_date: movie[4],
-      genre: movie[5],
-      duration: movie[6].to_i,
-      rating: rating_calculation(movie[7]),
-      director: movie[8],
-      cast: movie[9]
-    }
+    OpenStruct.new(link: movie[0],
+                   title: movie[1],
+                   shoot_year: movie[2].to_i,
+                   origin: movie[3],
+                   release_date: Date.parse(normalizing_dates(movie[4])),
+                   genre: movie[5],
+                   duration: movie[6].to_i,
+                   rating: rating_calculation(movie[7]),
+                   director: movie[8],
+                   cast: movie[9])
   end
 end
 
-def finding_movies(movies_hash, title)
-  movies_hash.select { |movie| movie[:title].include?(title) }
+def normalizing_dates(date)
+  case date.size
+  when 4
+    "#{date}-01-01"
+  when 7
+    "#{date}-01"
+  else
+    date
+  end
+end
+
+def finding_movies(movies, title)
+  movies.select { |movie| movie[:title].include?(title) }
 end
 
 def sorted_by_duration(movies)
@@ -47,15 +60,23 @@ def sorted_by_genre(movies, comedy)
 end
 
 def sorted_by_origin(movies, country)
-  sorted_by_year(movies).reject { |movie| movie[:origin_country].include?(country) }
+  sorted_by_year(movies).reject { |movie| movie[:origin].include?(country) }
 end
 
-def render_movie(movie)
-  puts "#{movie[:title]} #{movie[:rating]}"
+def sorted_by_month(movies)
+  sorted_by_year(movies).sort_by { |date| date[:release_date].mon }
+end
+
+def counted_by_months(movies)
+  sorted_by_month(movies).each_with_object(Hash.new(0)) { |date, hash| hash[date[:release_date].strftime('%B')] += 1 }
 end
 
 def rating_calculation(rating)
   '*' * ((rating.to_f - 8) * 10).round
+end
+
+def render_movie(movie)
+  puts "#{movie[:title]} #{movie[:rating]}"
 end
 
 def render_hash_movies(movie)
@@ -66,20 +87,29 @@ def render_directors(movie)
   puts movie[:director].split.last
 end
 
+def render_with_dates(key, value)
+  puts "#{key}: #{value}"
+end
+
 movies_data = reading_file(ARGV.first.to_s) # ARRAY -> string
 movies_array = normalizing_movie_data(movies_data) # string -> array with subarrays
-movies_hash = movie_to_hash(movies_array) # array with subarrays -> array with hashes
+movies_struct = movie_to_ostruct(movies_array) # array with subarrays -> array with OpenStruct
 
-finding_movies(movies_hash, 'Max').each { |movie| render_movie(movie) }
+puts "Movies which contain \"Max\" in title: \n"
+finding_movies(movies_struct, 'Max').each { |movie| render_movie(movie) }
 
 puts "5 longest movies: \n"
-sorted_by_duration(movies_hash).first(5).each { |movie| render_hash_movies(movie) }
+sorted_by_duration(movies_struct).first(5).each { |movie| render_hash_movies(movie) }
 
 puts "10 comedies sorted by year: \n"
-sorted_by_genre(movies_hash, 'Comedy').first(10).each { |movie| render_hash_movies(movie) }
+sorted_by_genre(movies_struct, 'Comedy').first(10).each { |movie| render_hash_movies(movie) }
 
 puts "Movies filmed outside USA: \n"
-sorted_by_origin(movies_hash, 'USA').each { |movie| render_hash_movies(movie) }
+sorted_by_origin(movies_struct, 'USA').each { |movie| render_hash_movies(movie) }
 
 puts "All directors in alphabetic order: \n"
-sorted_by_last_name(movies_hash).each { |movie| render_directors(movie) }
+sorted_by_last_name(movies_struct).each { |movie| render_directors(movie) }
+
+puts "Movies released overall in each month: \n"
+counted_by_months(movies_struct).each { |key, value| render_with_dates(key, value) }
+
