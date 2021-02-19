0a51a4f9fda250fadb0716f815c156f210bda5cf
koremori <ifyourelookingforhotwater@gmail.com>
2021-02-19 22:31:55 +0300

    rewrites in a few methods

--- imdb.rb
+++ imdb.rb
@@ -19,7 +19,7 @@ def movie_to_ostruct(movies) # => Array with OpenStruct
                    title: movie[1],
                    shoot_year: movie[2].to_i,
                    origin: movie[3],
-                   release_date: Date.parse(normalizing_dates(movie[4])),
+                   release_date: normalizing_dates(movie[4]),
                    genre: movie[5],
                    duration: movie[6].to_i,
                    rating: rating_calculation(movie[7]),
@@ -29,14 +29,14 @@ def movie_to_ostruct(movies) # => Array with OpenStruct
 end
 
 def normalizing_dates(date)
-  case date.size
-  when 4
-    "#{date}-01-01"
-  when 7
-    "#{date}-01"
-  else
-    date
-  end
+  Date.parse(case date.size
+             when 4
+               "#{date}-01-01"
+             when 7
+               "#{date}-01"
+             else
+               date
+             end
 end
 
 def finding_movies(movies, title)
@@ -56,15 +56,15 @@ def sorted_by_last_name(movies)
 end
 
 def sorted_by_genre(movies, comedy)
-  sorted_by_year(movies).select { |movie| movie[:genre].include?(comedy) }
+  movies.select { |movie| movie[:genre].include?(comedy) }
 end
 
 def sorted_by_origin(movies, country)
-  sorted_by_year(movies).reject { |movie| movie[:origin].include?(country) }
+  movies.reject { |movie| movie[:origin].include?(country) }
 end
 
 def sorted_by_month(movies)
-  sorted_by_year(movies).sort_by { |date| date[:release_date].mon }
+  movies.sort_by { |date| date[:release_date].mon }
 end
 
 def counted_by_months(movies)
@@ -94,6 +94,7 @@ end
 movies_data = reading_file(ARGV.first.to_s) # ARRAY -> string
 movies_array = normalizing_movie_data(movies_data) # string -> array with subarrays
 movies_struct = movie_to_ostruct(movies_array) # array with subarrays -> array with OpenStruct
+year_sorted_struct = sorted_by_year(movies_struct)
 
 puts "Movies which contain \"Max\" in title: \n"
 finding_movies(movies_struct, 'Max').each { |movie| render_movie(movie) }
@@ -102,14 +103,13 @@ puts "5 longest movies: \n"
 sorted_by_duration(movies_struct).first(5).each { |movie| render_hash_movies(movie) }
 
 puts "10 comedies sorted by year: \n"
-sorted_by_genre(movies_struct, 'Comedy').first(10).each { |movie| render_hash_movies(movie) }
+sorted_by_genre(year_sorted_struct, 'Comedy').first(10).each { |movie| render_hash_movies(movie) }
 
 puts "Movies filmed outside USA: \n"
-sorted_by_origin(movies_struct, 'USA').each { |movie| render_hash_movies(movie) }
+sorted_by_origin(year_sorted_struct, 'USA').each { |movie| render_hash_movies(movie) }
 
 puts "All directors in alphabetic order: \n"
 sorted_by_last_name(movies_struct).each { |movie| render_directors(movie) }
 
 puts "Movies released overall in each month: \n"
 counted_by_months(movies_struct).each { |key, value| render_with_dates(key, value) }
-
