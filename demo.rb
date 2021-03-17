21a3491f210ade91eafadfe8a2df17ddd243195e
koremori <ifyourelookingforhotwater@gmail.com>
2021-03-18 00:32:21 +0300

    initial commit

--- demo.rb
+++
@@ -1,9 +0,0 @@
-require '/home/koremori/Task_05/movie_collection.rb'
-require '/home/koremori/Task_05/movie.rb'
-
-ass = MovieCollection.new
-dick = ass.to_a
-#cock = Movie.new(dick)
-
-
-dick.each { |item| item.each { |elem| p elem[3] } }
